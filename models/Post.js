1dd37927be18dc175ede87d6d1dbb58f530cc1da
Deepa <kosalai17@gmail.com>
2021-08-06 00:19:43 -0700

    User model

---
+++ models/Post.js
@@ -0,0 +1,5 @@
+// import Model and Data types from Sequelize
+const { Model, DataTypes } = require('sequelize');
+// connection to db 
+const sequelize = require('../config/connection');
+
--- models/User.js
+++ models/User.js
@@ -8,13 +8,52 @@ const sequelize = require('../config/connection');
 
  //initialize the model's data and configuration
  User.init(
-     // Define Columns and the datatypes 
+    // Define Columns and the datatypes 
     {
-
+         // define an id column
+          id: {
+            // use the special Sequelize DataTypes object provide what type of data it is
+            type: DataTypes.INTEGER,
+            // this is the equivalent of SQL's `NOT NULL` option
+            allowNull: false,
+            // instruct that this is the Primary Key
+            primaryKey: true,
+            // turn on auto increment
+            autoIncrement: true
+          },
+          // define a username column
+          username: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+          // define an email column
+          email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // there cannot be any duplicate email values in this table
+            unique: true,
+            // if allowNull is set to false, we can run our data through validators before creating the table data
+            validate: {
+              isEmail: true
+            }
+          },
+          // define a password column
+          password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              // this means the password must be at least four characters long
+              len: [4]
+            }
+          }
     },
-    {
-        // Table configuration options
 
+    {// Table configuration options
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
     }
      
  )
