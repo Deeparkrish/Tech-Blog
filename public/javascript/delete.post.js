53073cdb6b79e518da97679bc1ed1e667d85ad12
Deepa <kosalai17@gmail.com>
2021-08-08 14:54:12 -0700

    add/edit and delete post

---
+++ public/javascript/delete.post.js
@@ -0,0 +1,26 @@
+async function deleteFormHandler(event) {
+    event.preventDefault();
+    // get  the correspondig  post id 
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+      ];
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE', // use DELETE  route  method 
+        body: JSON.stringify({
+          post_id: id
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      
+      if (response.ok) {
+        document.location.replace('/dashboard/'); // show the dashboard with post deleted 
+      } else {
+        alert(response.statusText);
+      }
+    
+  }
+  // Call the eventlistener function when delete button is clicked 
+  document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
---
+++ public/javascript/edit-post.js
@@ -0,0 +1,29 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+    // get the post title and content inputted in the text boxes 
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_content = document.querySelector('input[name="post-content"]').value;
+    // split the route path and fetch id 
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+      ];
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT', // PUT method to update the post 
+        body: JSON.stringify({
+            title,
+            content
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+      });
+      
+      if (response.ok) {
+        document.location.replace('/dashboard/'); // render the dashboard with  updated post
+      } else {
+        alert(response.statusText);
+      }
+  }
+// Event Listener when subnit button is clicked 
+  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
